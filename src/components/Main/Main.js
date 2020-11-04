c6c3618351483487a4fd555992be1b74e7c3077b
mukolakutsil <mukola.kutsil22@gmail.com>
2020-11-04 23:23:01 +0200

    create_components

---
+++ src/components/Main/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Main.css';
+
+const Main = (props) => {
+    return (
+        <>
+            <section className="main">
+                <h1 className="title">
+                    Falcon 1
+		        </h1>
+
+                <div className="video-container">
+                    <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
+
+                </div>
+            </section>
+        </>
+    )
+};
+
+export default Main;
