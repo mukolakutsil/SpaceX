7329df2b69154f64031f3e9cf6005ac207f0503d
mukolakutsil <mukola.kutsil22@gmail.com>
2020-11-12 13:29:00 +0200

    add_FetchData

--- src/App.js
+++ src/App.js
@@ -3,19 +3,51 @@ import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import FetchData from './service/FetchData';
 
 import './style.css';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Features/>
-      <Footer/>
-     
-    </>
-  );
+class App extends React.Component {
+  
+  fetchData = new FetchData();
+
+  state = {
+     rocket: 'Falcon 1',
+     rocketFeatures: null,
+     rockets: [],
+  };
+
+  componentDidMount(){
+    this.updateRocket();
+  }
+
+  updateRocket(){
+    this.fetchData.getRocket()
+    .then(data => {
+      this.setState({rockets: data.map(item => item.name)});
+      return data
+    })
+    .then(data => data.find(item => item.name === this.state.rocket))
+    .then(rocketFeatures => {this.setState({rocketFeatures})});
+  };
+  
+  changeRocket = rocket => {
+      this.setState({
+        rocket
+      }, this.updateRocket);
+  }
+
+  render() {
+    return (
+      <>
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket} />
+        <Main rocket={this.state.rocket}/>
+        <Features />
+        <Footer />
+
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,16 +2,27 @@ import React from 'react';
 
 import './Main.css';
 
+const video = {
+	'Falcon 1': 'moon',
+	'Falcon 9': 'earth',
+	'Falcon Heavy': 'mars',
+	other: 'space'
+}
+
 const Main = (props) => {
     return (
         <>
             <section className="main">
                 <h1 className="title">
-                    Falcon 1
+                    {props.rocket}
 		        </h1>
 
                 <div className="video-container">
-                    <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
+                    <video className="video" 
+                    autoPlay loop muted 
+                    src={`./video/${video.hasOwnProperty(props.rocket) ? 
+                    video[props.rocket] : 
+                    video.other}.mp4`}/>
 
                 </div>
             </section>
